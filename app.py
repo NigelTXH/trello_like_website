892ac8ece3857c967e3c139a5ef781750026a855
Brandon Yong <byon0002@student.monash.edu>
2022-09-03 19:05:19 +0800

    sign-up page skeleton added

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def test_page():
 def login_page():
     return render_template("login.html")
 
+@app.route("/signup")
+def signup_page():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
