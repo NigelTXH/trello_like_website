d111f4854e07f680fe7e447e1891b70c591cf610
Brandon Yong <byon0002@student.monash.edu>
2022-09-04 17:17:40 +0800

    Completed login form back end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -6,9 +6,15 @@ app = Flask(__name__)
 def test_page():
     return render_template("index.html")
 
-@app.route("/login")
-def login_page():
-    return render_template("login.html")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form.get("email") != "email@email.com" or request.form.get("password") != "password":
+            error = "Invalid credentials"
+        else:
+            return redirect("/")
+    return render_template('login.html', error=error)
 
 @app.route("/signup")
 def signup_page():
