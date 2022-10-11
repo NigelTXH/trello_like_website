eb2aa7171876d93f518ecd62ddc7a8f52c9a3083
Brandon Yong <byon0002@student.monash.edu>
2022-10-12 02:36:01 +0800

    update to timer

--- timer.py
+++ timer.py
@@ -1,6 +1,11 @@
-import tkinter
+from tkinter import *
 from datetime import datetime
 
 class Timer():
     
+    def __init__(self, starting_time):
+        self.counter = starting_time
+        self.running = False
+        self.main = True
+        
     
